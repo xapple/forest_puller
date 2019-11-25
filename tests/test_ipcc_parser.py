9494f0cb7753e328739bf290aeec3c95a98a6d29
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2019-11-25 14:06:37 +0100

    Specify ipcc access methods

--- tests/test_ipcc_parser.py
+++ tests/test_ipcc_parser.py
@@ -1,27 +1,34 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
-
 
 """
 Written by Lucas Sinclair and Paul Rougieux.
 
-JRC Biomass Project.
+JRC biomass Project.
 Unit D1 Bioeconomy.
 
-Test methods parsing data from the IPCC Common Reporting FOrmat (CRF) table 4.
+Test methods parsing data from the IPCC Common Reporting Format (CRF) table 4.
 """
+
+# Built-in modules #
+
 # First party modules #
 from plumbing.dataframes import string_to_df
 
 # Expected data #
-crf = string_to_df("""protection_status | area  | gain | loss | stock_change|
-                      merch             | 0.7   | 0.3  | 0.3  | 0.3         |
-                      prod_irw          | 0     | 1    | 0.3  | 0.3         |""")
-
+crf = string_to_df("""country | year | land_use    | subdivision | area | gain | loss | stock_change |
+                              |      |             |             |      |      |      |              |
+                           AT | 2017 | forest_land | coniferous  | 0.7  | 0.3  | 0.3  | 0.3          |
+                           AT | 2017 | forest_land | deciduous   | 0    | 1    | 0.3  | 0.3          |""")
 
 ###############################################################################
-def test_parse_austria_2000():
+def test_parse():
     """
-    Load IPCC CRF data for austria
+    Load IPCC CRF data for Austria.
     """
-    ipcc_crf.download
+    # Import #
+    from forest_puller.ipcc import dataset as ipcc
+    # Check one value #
+    index  = ['country', 'year', 'land_use',    'subdivision']
+    values = ['AT',      '2017', 'forest_land', 'coniferous']
+    assert ipcc.set_index(index).loc[values, 'stock_change'][0] == 0.3
