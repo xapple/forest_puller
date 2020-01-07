3e71f27f531723d94578dcc4c07cb38a28f9fe40
Paul Rougieux <paul.rougieux@gmail.com>
2020-01-07 12:32:11 +0100

    Extract header information

--- forest_puller/ipcc/crf.py
+++ forest_puller/ipcc/crf.py
@@ -18,12 +18,19 @@ Typically you can use this class this like:
 # Internal modules #
 from forest_puller import cache_dir as cache_dir_package
 from forest_puller.ipcc.downloads import downloads
+from forest_puller import module_dir
 
 # First party modules #
 from plumbing.cache import property_cached
 
 # Third party modules #
 from tqdm import tqdm
+import pandas
+
+# Load IPCC column name mapping to short names
+ipcc_column_mapping_file = module_dir + 'variable_mapping/ipcc_columns.csv'
+ipcc_column_mapping = pandas.read_csv(str(ipcc_column_mapping_file))
+
 
 ###############################################################################
 class IPCC_CRF:
@@ -62,6 +69,44 @@ class IPCC_CRF:
         """
         for i, row in tqdm(downloads.df.iterrows()):
             print(row['zip'])
+
+
+    def parse_table_4(self, excel_file_path):
+        """ Extract information from table 4 into a pandas data frame"""
+        # Parameters for the position of rows used for the header
+        # zero-index (offset of one compared to the excel row numbers)
+        begin_head = 4
+        end_head = 9
+        # Load table into pandas
+        table4 = pandas.read_excel(excel_file_path,
+                                   sheet_name='Table4.A',
+                                   header=None,
+                                   na_values=['IE','NE','NO','NO,NE'])
+        # Extract headers
+        header = table4.iloc[begin_head:end_head]
+        header = header.fillna(method='ffill')
+        header = header.reset_index(drop=True)
+        # Add information on per area columns in t C /ha
+        # so they have different name than the kt C columns
+        header.iloc[3,5:12] = header.iloc[3,5:12] + '_per_area'
+        # Convert to short headers
+        # Check column names for the reason why this doesn't work properly
+        header = header.iloc[3].replace(list(ipcc_column_mapping['ipcc']),
+                                        list(ipcc_column_mapping['forest_puller']))
+
+        # Exctract table body
+        # Look for the position of the first mostly empty row
+        # at the end of the table in order to remove all linkes after that
+        selector = table4.isnull().sum(axis=1) > 18
+        selector[0:end_head] = False
+        selector = selector.cumsum() == 0
+        last_row = max(selector.index[selector])
+        df = table4.iloc[end_head+1:last_row +1]
+        # Rename columns
+        df.columns = header
+        # Return
+        return df
+
 
 ###############################################################################
 # Create a singleton #
---
+++ notes.md
@@ -0,0 +1,14 @@
+
+
+
+
+# Units
+
+Example variable descriptions
+
+  variable,name_in_ipcc,name_in_faostat,unit
+  area,forest area, area_forested,hectare
+  emissions,total emissions, NA, tons of carbon
+
+
+
