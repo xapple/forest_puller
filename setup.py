ec242ec146f6c29ee29b43405f6fa68ab129b3ef
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2020-01-09 15:09:47 +0100

    Rewrite description

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
         name             = 'forest_puller',
         version          = '0.1.0',
-        description      = 'A package for retrieving data concerning forest growth and stock on the European continent.',
+        description      = 'A package for retrieving data concerning forests on the European continent.',
         long_description = open('README.md').read(),
         license          = 'MIT',
         url              = 'https://gitlab.com/bioeconomy/puller/forest_puller',
@@ -11,4 +11,4 @@ setup(
         author_email     = 'lucas.sinclair@me.com',
         packages         = find_packages(),
         install_requires = ['pandas', 'autopaths', 'plumbing', 'lxml', 'requests'],
-    )
+)
