4583f0949cde895ebf389a02817b8279ab04000a
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2020-01-13 13:41:18 +0100

    Rewrite parsing of table 4

--- forest_puller/ipcc/year.py
+++ forest_puller/ipcc/year.py
@@ -28,15 +28,26 @@ col_name_map = pandas.read_csv(str(col_name_map))
 class Year:
     """
     Represents a specific year from a specific country's dataset.
+
+    Here the numbers are parameters for the position of rows used for
+    the header. It is zero-indexed (i.e. offset of one compared to the
+    GUI excel row numbers)
     """
 
+    begin_head = 4
+    end_head   = 9
+
     def __init__(self, country, xls_file):
+        """
+        Record the parent and the file we have to parse.
+        """
         # The parent country #
         self.country = country
         # The file that contains data for this year #
         self.xls_file = xls_file
         # The year itself from the file name #
         self.year = int(re.findall("^[A-Z]+_[0-9]+_([0-9]+)", xls_file.name)[0])
+        # Position #
 
     def __repr__(self):
         return '%s %s of %s' % (self.__class__, self.year, self.country.iso2_code)
@@ -44,7 +55,7 @@ class Year:
     # ------------------------------- Other ----------------------------------#
     @property_cached
     def raw_table_4a(self):
-        """Table4.A without any modifications."""
+        """Table4.A as is without any modifications."""
         # Load table #
         df = pandas.read_excel(str(self.xls_file),
                                sheet_name = 'Table4.A',
@@ -54,41 +65,34 @@ class Year:
         return df
 
     @property_cached
-    def header(self):
-        """
-        Parameters for the position of rows used for the header.
-        It is zero-indexed (i.e. offset of one compared to the
-        GUI excel row numbers)
-        """
-        # Position #
-        begin_head = 4
-        end_head   = 9
-        header     = self.raw_table_4a.iloc[begin_head:end_head]
+    def column_names(self):
+        # Raw header #
+        df = self.raw_table_4a.iloc[self.begin_head:self.end_head]
         # Fill values #
-        header = header.fillna(method='ffill')
-        header = header.reset_index(drop=True)
+        df = df.fillna(method='ffill')
+        df = df.reset_index(drop=True)
         # Add information on per area columns in t C /ha
         # so they have different name than the kt C columns
-        header.iloc[3, 5:12] = header.iloc[3,5:12] + '_per_area'
+        df.iloc[3, 5:12] = df.iloc[3,5:12] + '_per_area'
         # Convert to short headers
         # Check column names for the reason why this doesn't work properly
-        header = header.iloc[3].replace(list(col_name_map['ipcc']),
-                                        list(col_name_map['forest_puller']))
+        df = df.iloc[3].replace(list(col_name_map['ipcc']),
+                                list(col_name_map['forest_puller']))
         # Return #
-        return header
+        return df
 
     @property_cached
     def df(self):
         """Extract targeted information from 'Table4.A' into a pandas data frame."""
-        # Extract table body
         # Look for the position of the first mostly empty row
-        # at the end of the table in order to remove all links after that
+        # at the end of the table
         selector = self.raw_table_4a.isnull().sum(axis=1) > 18
-        selector[0:end_head] = False
+        selector[0:self.end_head] = False
         selector = selector.cumsum() == 0
         last_row = max(selector.index[selector])
-        df       = self.raw_table_4a.iloc[end_head+1:last_row+1]
-        # Rename columns #
-        df.columns = self.header
+        # Take all lines after the header but before the last row #
+        df = self.raw_table_4a.iloc[self.end_head + 1:last_row + 1]
+        # Rename columns index #
+        df.columns = self.column_names
         # Return #
         return df
