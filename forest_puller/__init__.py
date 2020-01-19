410be3877af736449ff7383ba54a28d31620da9d
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2020-01-19 17:24:49 +0100

    Version 1.0.0

--- forest_puller/__init__.py
+++ forest_puller/__init__.py
@@ -9,7 +9,7 @@ Unit D1 Bioeconomy.
 """
 
 # Special variables #
-__version__ = '0.1.1'
+__version__ = '1.0.0'
 
 # Built-in modules #
 import os, sys
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
         name             = 'forest_puller',
-        version          = '0.1.1',
+        version          = '1.0.0',
         description      = 'A package for retrieving data concerning forests on the European continent.',
         long_description = open('README.md').read(),
         license          = 'MIT',
@@ -10,5 +10,5 @@ setup(
         author           = 'Lucas Sinclair',
         author_email     = 'lucas.sinclair@me.com',
         packages         = find_packages(),
-        install_requires = ['pandas', 'autopaths', 'plumbing', 'lxml', 'requests', 'tqdm'],
+        install_requires = ['pandas', 'numpy', 'autopaths', 'plumbing', 'lxml', 'requests', 'tqdm'],
 )
