e279af66202ecc834472bf92c091c5c574c7d20e
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2020-01-19 17:25:29 +0100

    Back to development version.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# `forest_puller` version 1.0.1
+# `forest_puller` version 1.0.2
 
 `forest_puller` is a package for retrieving data concerning forests on the European continent. This includes forest growth rates, amount of forested areas and forest inventory (standing stock).
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
         name             = 'forest_puller',
-        version          = '1.0.1',
+        version          = '1.0.2',
         description      = 'A package for retrieving data concerning forests on the European continent.',
         long_description = open('README.md').read(),
         license          = 'MIT',
