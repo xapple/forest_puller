07e198d6373a9ffed3c666ebf1fbe1544fd0440e
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2020-01-21 15:18:15 +0100

    Added ipcc.concat with a large dataframe containing everything (bis)

---
+++ forest_puller/ipcc/concat.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Written by Lucas Sinclair and Paul Rougieux.
+
+JRC biomass Project.
+Unit D1 Bioeconomy.
+"""
+
+# Built-in modules #
+
+# Internal modules #
+from forest_puller.ipcc.country import all_countries
+
+# First party modules #
+
+# Third party modules #
+import pandas
+
+##############################################################################
+every_year = (y.year_country_cols for c in all_countries for y in c)
+df = pandas.concat(every_year)
