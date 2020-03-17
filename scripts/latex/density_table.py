e92f34f20b56f856af51f75f9327d096cb00af95
Paul Rougieux <paul.rougieux@gmail.com>
2020-03-17 15:42:47 +0100

    Update column titles

--- scripts/latex/density_table.py
+++ scripts/latex/density_table.py
@@ -11,7 +11,12 @@ Script to automatically generate a latex formatted table from a pandas data fram
 
 Typically you would run this file from a command line like this:
 
-     ipython3 -i -- ~/deploy/forest_puller/scripts/latex/density_table.py
+    ipython3 -i -- ~/deploy/forest_puller/scripts/latex/density_table.py
+
+Or like this:
+
+    ipython3 -i -- ~/repos/forest_puller/scripts/latex/density_table.py
+
 """
 
 # Built-in modules #
@@ -23,25 +28,33 @@ from forest_puller.soef.composition import composition_data
 from autopaths import Path
 
 # Third party modules #
-import pandas
 
 ###############################################################################
 # Load #
 df = composition_data.avg_densities.copy()
 
 # Downcast to integer #
-df['year'] = df['year'].apply(pandas.to_numeric, errors="coerce", downcast='integer')
+df['year'] = df['year'].apply(int)
+# Make frac missing in percent
+df['frac_missing'] = df['frac_missing'] * 100
 
 # Pivot #
-avg_densities_wide = df.pivot(index = 'country', columns='year', values=['avg_density', 'frac_missing'])
+df = df.pivot(index = 'country', columns='year', values=['avg_density', 'frac_missing'])
+
+# Rename columns
+df = df.rename(columns={'avg_density':'Average density ($kg/m^3$)',
+                        'frac_missing':'Fraction missing (percent)'})
 
 # Pick the title #
-label = 'Weighted average density by country'
+label = 'average_density'
+caption = 'Weighted average density by country'
 
 # Generate LaTeX #
-tex = avg_densities_wide.to_latex(float_format = "%.3f",
-                                  na_rep       = '',
-                                  label        = label)
+tex = df.to_latex(float_format = "%.0f",
+                  na_rep       = '',
+                  escape       = False,
+                  label        = label,
+                  caption      = caption)
 
 # Pick the destination #
 #path = Path("/Users/sinclair/repos/sinclair/work/ispra_italy/repos/puller_pub/tables/density_table.tex")
