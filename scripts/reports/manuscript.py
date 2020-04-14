750969d7c6c1fcb0aff29dd8e219e3dd9fe89606
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2020-04-14 19:46:46 +0200

    Add script to generate manuscript figures

---
+++ scripts/reports/manuscript.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Written by Lucas Sinclair and Paul Rougieux.
+
+JRC Biomass Project.
+Unit D1 Bioeconomy.
+
+Script to generate all the figures that are later used in the manuscript.
+
+Typically you would run this file from a command line like this:
+
+     ipython3 -i -- ~/deploy/forest_puller/scripts/reports/manuscript.py
+"""
+
+# Built-in modules #
+
+# Third party modules #
+from tqdm import tqdm
+
+###############################################################################
+from forest_puller.viz.manuscript.area_comparison import legend
+legend.plot(rerun=True)
+
+from forest_puller.viz.manuscript.area_comparison import all_graphs
+for g in tqdm(all_graphs): g.plot(rerun=True)
+
+from forest_puller.viz.manuscript.dynamics_comparison import all_graphs
+for g in tqdm(all_graphs): g.plot(rerun=True)
+
