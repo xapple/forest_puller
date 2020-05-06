34ff137596610f6acd8866232951c1127d7742bc
Paul Rougieux <paul.rougieux@gmail.com>
2020-05-06 18:54:38 +0200

    Add ipcc bcef table, one per zone because growing stock columns are different

--- forest_puller/soef/test_expansion_factor.py
+++ forest_puller/soef/test_expansion_factor.py
@@ -26,12 +26,19 @@ def test_split_unknown_forest_type():
     # Initialize input values
     remaining_forest_types = ['con', 'broad']
     df = pandas.DataFrame({'area': [10,20,30],
-                           'con_broad':['con','other','broad']})
+                           'forest_type':['con','other','broad']})
     # Expected output
     df_expected = pandas.DataFrame({'area': [20,40],
-                                    'con_broad':['con','broad']})
+                                    'forest_type':['con','broad']})
     # Call the function
-    df_con_broad = expansion_factor.split_unknown_forest_type(df, remaining_forest_types )
+    df_con_broad = expansion_factor.split_unknown_forest_type(df, remaining_forest_types)
     # Test
     assert_frame_equal(df_con_broad, df_expected)
+
+
+def test_compute_bcef():
+    """Test the function that aggregates and compute the
+    biomass conversion and expansion factors
+    """
+
 
