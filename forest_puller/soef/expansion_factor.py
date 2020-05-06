cda0fa0f75007a56db9268566039978a716f811f
Paul Rougieux <paul.rougieux@gmail.com>
2020-05-06 18:03:10 +0200

    Add test case and script to prepare for the expansion factor

---
+++ forest_puller/soef/expansion_factor.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  6 17:10:11 2020
+
+@author: paul
+"""
+
+
+def split_unknown_forest_type(df, remaining_forest_types):
+    """
+
+
+    Parameters
+    ----------
+    df : data frame containing a forest_type column
+    remaining_forest_types : forest types that should remain
+        in the output data frame
+
+    Returns
+    -------
+    df : data frame
+
+    """
+
+    return df
---
+++ forest_puller/soef/test_expansion_factor.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""@author: Paul Rougieux and Lucas Sinclair
+
+Execute the test suite from bash with py.test as follows:
+
+    cd ~/repos/forest_puller/forest_puller/soef
+    pytest
+    # enter debugger on failure
+    pytest --pdb
+
+"""
+
+# Third party modules
+import pandas
+from pandas.testing import assert_frame_equal
+# Project modules
+import expansion_factor
+
+
+def test_split_unknown_forest_type():
+    """
+    Test the function that splits the unknown or mixed forest type to
+    the given categories, for example coniferous and broad leaves.
+    """
+    # Initialize input values
+    remaining_forest_types = ['con', 'broad']
+    df = pandas.DataFrame({'area': [10,20,30],
+                           'con_broad':['con','other','broad']})
+    # Expected output
+    df_expected = pandas.DataFrame({'area': [20,40],
+                                    'con_broad':['con','broad']})
+    # Call the function
+    df_con_broad = expansion_factor.split_unknown_forest_type(df, remaining_forest_types )
+    # Test
+    assert_frame_equal(df_con_broad, df_expected)
+
