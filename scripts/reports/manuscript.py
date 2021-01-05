3a972e34bbe65e957b51fb274779f9952dc25fd7
Paul Rougieux <paul.rougieux@gmail.com>
2021-01-05 15:08:15 +0100

    Update average growth table used in the publication

--- scripts/reports/manuscript.py
+++ scripts/reports/manuscript.py
@@ -57,13 +57,12 @@ print(max_area.save())
 from forest_puller.tables.available_for_supply import afws_comp
 print(afws_comp.save())
 
-# TODO: generates an error, see if avg_tons_pub is needed
-#ImportError: cannot import name 'avg_tons_pub' from 'forest_puller.tables.average_growth' (/home/paul/repos/forest_puller/forest_puller/tables/average_growth.py)
-#from forest_puller.tables.average_growth import avg_tons_pub
-#print(avg_tons_pub.save())
+from forest_puller.tables.average_growth import avg_tons
+print(avg_tons.save())
 
 # Save manuscript data for use in other statistical software
 # TODO replace ~/downloads by an appropriate folder under cache_dir
+# Or use a similar save methods as used for the above tables
 from forest_puller.viz.increments_df import increments_data
 increments_data.df.to_csv("~/downloads/gains_loss_net_mixed_units.csv", index=False)
 from forest_puller.viz.converted_to_tons import converted_tons_data
